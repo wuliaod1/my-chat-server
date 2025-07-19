ede84bd8b0bf2351b883374e033b0224aa759817
wuliaod1 <3093209984@qq.com>
2025-07-20 00:21:38 +0800

    Update user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -1,7 +1,8 @@
+// 文件路径: /models/user.model.js
+
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
-// 好友请求的子文档结构
 const friendRequestSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     username: String,
@@ -11,10 +12,14 @@ const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true, trim: true },
     password: { type: String, required: true },
     friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-    friendRequests: [friendRequestSchema]
+    friendRequests: [friendRequestSchema],
+    // --- 新增字段 ---
+    pushSubscription: {
+        type: Object,
+        required: false
+    }
 }, { timestamps: true });
 
-// 在保存用户前，如果密码被修改，则进行哈希加密
 userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) return next();
     const salt = await bcrypt.genSalt(10);
@@ -22,7 +27,6 @@ userSchema.pre('save', async function (next) {
     next();
 });
 
-// 实例方法：比对输入的密码和数据库中加密的密码
 userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
