a2b18c9e2c32f2d103fcd3607608415b3364ffde
wuliaod1 <3093209984@qq.com>
2025-07-19 13:25:24 +0800

    Delete src directory

--- src/config/index.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-  databaseUrl: process.env.DATABASE_URL,
-  jwtSecret: process.env.JWT_SECRET || 'supersecretjwtkey',
-  port: process.env.PORT || 3000,
-};
